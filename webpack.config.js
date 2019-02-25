fd0d7cb0198af0c739886a781ede778593a98c27
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2019-02-25 13:02:47 -0300

    Add plugins and config mode production or develop:
    - Cross-env
    - optimize-css-assets-webpack-plugin
    - terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
+const modoDev = process.env.NODE_ENV !== "production";
 const webpack = require("webpack");
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 /*
  * 1º Dizer se tá no "mode" "development" ou "production" e indicar o "entry"
  * 2º Configurar o "output" "output.filename" "output.path"
  * 3º Add loader
+ * 4º Add cross-env
  */
 
 module.exports = {
-  mode: "development",
+  mode: modoDev ? "development" : "production",
   entry: "./src/main.js",
   output: {
     filename: "main.js",
     path: __dirname + "/public"
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
